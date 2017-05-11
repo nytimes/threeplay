018f40a768a70fc72bc30acdb27ee89799f1a7c2
Thiago Pontes <github@thiago.me>
2017-05-11 18:21:36 -0400

    checkpoint

--- threeplay_test.go
+++ threeplay_test.go
@@ -33,13 +33,39 @@ func (c *HTTPClientMock) PostForm(url string, data url.Values) (*http.Response, 
 }
 
 func TestGetFile(t *testing.T) {
-	c := &HTTPClientMock{}
+	httpClient := &HTTPClientMock{}
 	expectedApiCall := "https://api.3playmedia.com/files/123456?apikey=api-key"
 	c.On("Get", expectedApiCall).Return(createResponseFromJsonFile("./fixtures/file.json"), nil)
 	client := NewClientWithHTTPClient("api-key", "", c)
 	file, _ := client.GetFile(123456)
 	assert.Equal(t, file.Name, "72397_1_08macron-speech_wg_360p.mp4")
-	c.AssertExpectations(t)
+	httpClient.AssertExpectations(t)
+}
+
+func TestGetFiles(t *testing.T) {
+	httpClient := &HTTPClientMock{}
+	expectedApiCall := "https://api.3playmedia.com/files?apikey=api-key"
+	httpClient.On("Get", expectedApiCall).Return(createResponseFromJsonFile("./fixtures/files_page1.json"), nil)
+	client := NewClientWithHTTPClient("api-key", httpClient)
+
+	filesPage, _ := client.GetFiles()
+
+	assert.Equal(t, len(filesPage.Files), 10)
+	assert.Equal(t, filesPage.Summary.CurrentPage.String(), "1")
+	assert.Equal(t, filesPage.Summary.PerPage.String(), "10")
+	httpClient.AssertExpectations(t)
+}
+
+func TestGetFilesWithPagination(t *testing.T) {
+	httpClient := &HTTPClientMock{}
+	expectedApiCall := "https://api.3playmedia.com/files?apikey=api-key&page=2"
+	httpClient.On("Get", expectedApiCall).Return(createResponseFromJsonFile("./fixtures/files_page2.json"), nil)
+	client := NewClientWithHTTPClient("api-key", httpClient)
+	querystring := url.Values{}
+	querystring.Add("page", "2")
+	filesPage, _ := client.GetFiles(querystring)
+	assert.Equal(t, "2", filesPage.Summary.CurrentPage.String())
+	httpClient.AssertExpectations(t)
 }
 
 func TestUploadFileFromURL(t *testing.T) {
