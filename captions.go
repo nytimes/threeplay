8f03b060edd5930f300663be84b5070580e3a773
Ke Xu <k.xu@nytimes.com>
2017-05-18 23:42:48 -0400

    add support for captions api

---
+++ captions.go
@@ -0,0 +1,80 @@
+package threeplay
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+// CaptionsFormat is supported output format for captions
+type CaptionsFormat string
+
+const (
+	// SRT format for captions file
+	SRT CaptionsFormat = "srt"
+	// DFX format for captions file
+	DFX CaptionsFormat = "pdfxp"
+	// SMI format for captions file
+	SMI CaptionsFormat = "smi"
+	// STL format for captions file
+	STL CaptionsFormat = "stl"
+	// QT format for captions file
+	QT CaptionsFormat = "qt"
+	// QTXML format for captions file
+	QTXML CaptionsFormat = "qtxml"
+	// CPTXML format for captions file
+	CPTXML CaptionsFormat = "cptxml"
+	// ADBE format for captions file
+	ADBE CaptionsFormat = "adbe"
+)
+
+// GetCaptions get captions by threeplay file ID with specific format
+// current supported formats are srt, dfxp, smi, stl, qt, qtxml, cptxml, adbe
+func (c *Client) GetCaptions(fileID uint, format CaptionsFormat) ([]byte, error) {
+	endpoint := fmt.Sprintf("https://%s/files/%d/captions.%s?apikey=%s",
+		threePlayStaticHost, fileID, format, c.apiKey)
+
+	response, err := c.httpClient.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	apiError := &Error{}
+	json.Unmarshal(responseData, apiError)
+	if apiError.IsError {
+		return nil, errors.New("API Error")
+	}
+
+	return responseData, nil
+}
+
+// GetCaptionsByVideoID get captions by video ID with specific format
+// current supported formats are srt, dfxp, smi, stl, qt, qtxml, cptxml, adbe
+func (c *Client) GetCaptionsByVideoID(id string, format CaptionsFormat) ([]byte, error) {
+	endpoint := fmt.Sprintf("https://%s/files/%s/captions.%s?apikey=%s&usevideoid=1",
+		threePlayStaticHost, id, format, c.apiKey)
+
+	response, err := c.httpClient.Get(endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	apiError := &Error{}
+	json.Unmarshal(responseData, apiError)
+	if apiError.IsError {
+		return nil, errors.New("API Error")
+	}
+
+	return responseData, nil
+}
---
+++ captions_test.go
@@ -0,0 +1,77 @@
+package threeplay_test
+
+import (
+	"testing"
+
+	"github.com/NYTimes/threeplay"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
+)
+
+func TestGetCaptions(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New("https://static.3playmedia.com").
+		Get("/files/123456/captions.srt").
+		MatchParam("apikey", "api-key").
+		Reply(200).
+		File("./fixtures/captions.srt")
+
+	client := threeplay.NewClient("api-key", "secret-key")
+	result, err := client.GetCaptions(123456, threeplay.SRT)
+	assert.NotNil(result)
+	assert.Nil(err)
+}
+
+func TestGetCaptionsApiError(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New("https://static.3playmedia.com").
+		Get("/files/123456/captions.srt").
+		MatchParam("apikey", "api-key").
+		Reply(200).
+		File("./fixtures/error.json")
+
+	client := threeplay.NewClient("api-key", "secret-key")
+	result, err := client.GetCaptions(123456, threeplay.SRT)
+	assert.Nil(result)
+	assert.NotNil(err)
+	assert.Equal(err.Error(), "API Error")
+}
+
+func TestGetCaptionsByVideoID(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New("https://static.3playmedia.com").
+		Get("/files/123456/captions.srt").
+		MatchParam("apikey", "api-key").
+		MatchParam("usevideoid", "1").
+		Reply(200).
+		File("./fixtures/captions.srt")
+
+	client := threeplay.NewClient("api-key", "secret-key")
+	result, err := client.GetCaptionsByVideoID("123456", threeplay.SRT)
+	assert.NotNil(result)
+	assert.Nil(err)
+}
+
+func TestGetCaptionsByVideoIDApiError(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New("https://static.3playmedia.com").
+		Get("/files/123456/captions.srt").
+		MatchParam("apikey", "api-key").
+		MatchParam("usevideoid", "1").
+		Reply(200).
+		File("./fixtures/error.json")
+
+	client := threeplay.NewClient("api-key", "secret-key")
+	result, err := client.GetCaptionsByVideoID("123456", threeplay.SRT)
+	assert.Nil(result)
+	assert.NotNil(err)
+	assert.Equal(err.Error(), "API Error")
+}
