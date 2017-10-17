61e842cef3e7043c8c5f536921b9fb2e5e946970
Francisco Souza <francisco.souza@nytimes.com>
2017-10-17 16:11:33 -0400

    Support custom output formats on GetCaptions
    3play API is kinda weird in this situation, so the paths for custom and
    standard formats are different.
    
    The custom format is in the form "<id>.<extension>". The included test
    has an example.

--- captions_test.go
+++ captions_test.go
@@ -9,19 +9,108 @@ import (
 )
 
 func TestGetCaptions(t *testing.T) {
-	assert := assert.New(t)
-	defer gock.Off()
+	var tests = []struct {
+		name   string
+		opts   threeplay.GetCaptionsOptions
+		path   string
+		params map[string]string
+	}{
+		{
+			"with file id and standard format",
+			threeplay.GetCaptionsOptions{
+				FileID: 123456,
+				Format: threeplay.SRT,
+			},
+			"/files/123456/captions.srt",
+			map[string]string{"apikey": "api-key"},
+		},
+		{
+			"with file id and custom format",
+			threeplay.GetCaptionsOptions{
+				FileID:       123456,
+				OutputFormat: "42.srt",
+			},
+			"/files/123456/output_formats/42.srt",
+			map[string]string{"apikey": "api-key"},
+		},
+		{
+			"with video id and standard format",
+			threeplay.GetCaptionsOptions{
+				VideoID: "vid-123",
+				Format:  threeplay.SRT,
+			},
+			"/files/vid-123/captions.srt",
+			map[string]string{"apikey": "api-key", "usevideoid": "1"},
+		},
+		{
+			"with video id and custom format",
+			threeplay.GetCaptionsOptions{
+				VideoID:      "vid-123",
+				OutputFormat: "42.vtt",
+			},
+			"/files/vid-123/output_formats/42.vtt",
+			map[string]string{"apikey": "api-key", "usevideoid": "1"},
+		},
+		{
+			"with all fields - should prefer custom format && file id",
+			threeplay.GetCaptionsOptions{
+				FileID:       123456,
+				VideoID:      "vid-123",
+				Format:       threeplay.WebVTT,
+				OutputFormat: "42.srt",
+			},
+			"/files/123456/output_formats/42.srt",
+			map[string]string{"apikey": "api-key"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			defer gock.Off()
 
-	gock.New("https://static.3playmedia.com").
-		Get("/files/123456/captions.srt").
-		MatchParam("apikey", "api-key").
-		Reply(200).
-		File("./fixtures/captions.srt")
+			gock.New("https://static.3playmedia.com").
+				Get(test.path).
+				MatchParams(test.params).
+				Reply(200).
+				File("./fixtures/captions.srt")
 
-	client := threeplay.NewClient("api-key", "secret-key")
-	result, err := client.GetCaptions(123456, threeplay.SRT)
-	assert.NotNil(result)
-	assert.Nil(err)
+			client := threeplay.NewClient("api-key", "secret-key")
+			result, err := client.GetCaptions(test.opts)
+			assert.NotNil(result)
+			assert.Nil(err)
+		})
+	}
+}
+
+func TestGetCaptionsApiInvalidOptions(t *testing.T) {
+	var tests = []struct {
+		name string
+		opts threeplay.GetCaptionsOptions
+	}{
+		{
+			"missing format",
+			threeplay.GetCaptionsOptions{
+				FileID:  10,
+				VideoID: "vid-123",
+			},
+		},
+		{
+			"missing id",
+			threeplay.GetCaptionsOptions{
+				Format:       threeplay.SRT,
+				OutputFormat: "42.srt",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+			client := threeplay.NewClient("api-key", "secret-key")
+			result, err := client.GetCaptions(test.opts)
+			assert.Nil(result)
+			assert.NotNil(err)
+		})
+	}
 }
 
 func TestGetCaptionsApiError(t *testing.T) {
@@ -35,7 +124,10 @@ func TestGetCaptionsApiError(t *testing.T) {
 		File("./fixtures/error.json")
 
 	client := threeplay.NewClient("api-key", "secret-key")
-	result, err := client.GetCaptions(123456, threeplay.SRT)
+	result, err := client.GetCaptions(threeplay.GetCaptionsOptions{
+		FileID: 123456,
+		Format: threeplay.SRT,
+	})
 	assert.Nil(result)
 	assert.NotNil(err)
 	assert.Equal(threeplay.ErrUnauthorized.Error(), err.Error())
